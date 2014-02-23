6822361ca9c5e1bd0195e8519ae22a76c4ebeaea
ricmatsui <ricmatsui@gmail.com>
2014-02-22 17:08:21 -0800

    Added jade, static, templates

--- web.js
+++ web.js
@@ -2,12 +2,18 @@
 require('newrelic')
 var express = require("express");
 var logfmt = require("logfmt");
+var path = require('path');
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.favicon());
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index')
 });
 
 var port = Number(process.env.PORT || 5000);
