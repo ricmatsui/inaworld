b1ae672f9012e036659e8d73294403521c32990e
ricmatsui <ricmatsui@gmail.com>
2014-02-22 16:21:32 -0800

    Added new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['In A World'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'e8330cad3a26f45fe7e6b2b9f0e147d61cfa8d7d',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 // web.js
+require('newrelic')
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
